fed163d24bad1f721c53cad9a5f6fa5432c83a3e
rballachay <riley.ballachay@gmail.com>
2024-03-27 09:37:03 -0400

    update clinical bert training

--- src/medBERT/clinical_bert.py
+++ src/medBERT/clinical_bert.py
@@ -3,23 +3,41 @@ import json
 from tqdm import tqdm
 from src.utils import break_text_into_passages, format_prompt
 import torch
+from torch import nn
+from torch.nn import CrossEntropyLoss
 import re 
+
+class ClinicalBertQA(nn.Module):
+    def __init__(self):
+        super(ClinicalBertQA, self).__init__()
+        
+        # Load the tokenizer
+        self.tokenizer = AutoTokenizer.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
+        self.bert = AutoModel.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
+
+        self.pred_layer = nn.Linear(self.bert.config.hidden_size, 2)
+
+    def forward(self, inputs):
+        """this method is borrowed from from here:
+        https://huggingface.co/transformers/v3.0.2/_modules/transformers/modeling_bert.html#BertForQuestionAnswering
+        """
+        tokens = self.tokenizer(inputs, padding=True, max_length=512, truncation=True, return_tensors='pt').to(device)
+        outputs = self.bert(**tokens)
+        sequence_output = outputs[0]
+
+        logits = self.pred_layer(sequence_output)
+        start_logits, end_logits = logits.split(1, dim=-1)
+
+        start_logits = start_logits.squeeze(-1)
+        end_logits = end_logits.squeeze(-1)
+
+        outputs = (start_logits, end_logits,) #+ outputs[2:]
+        return outputs # (start_logits, end_logits)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def run_clinical_bert():
-
-    # Load the tokenizer
-    tokenizer = AutoTokenizer.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
-
-    # Load the model
-    model = AutoModel.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
-
-    #tokenizer_qa = AutoModelForSeq2SeqLM.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
-    tokenizer_qa = AutoTokenizer.from_pretrained("facebook/bart-large")
-    model_qa = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-large")
-
-    #qa_pipeline= pipeline("text-generation", model="yikuan8/Clinical-Longformer", tokenizer="yikuan8/Clinical-Longformer")
+    model = ClinicalBertQA()
 
     with open("data/LongHealth/data/benchmark_v5.json", "r") as f:
         benchmark = json.load(f)
@@ -27,9 +45,11 @@ def run_clinical_bert():
     for idx, patient in tqdm(benchmark.items(), position=0):
         # patient texts are a dictionary of texts labelled like 
         # text_0, text_1 ...
-        patient_doc_matrix, patient_document = get_patient_doc_matrix(patient, model, tokenizer, device)
+        
+        patient_text_lengths = {t:len(t) for t in patient['texts']}
 
-        patient_results = {}
+        patient_doc_matrix, patient_document = get_patient_doc_matrix(patient, model, device)
+
         for i, question in tqdm(
             enumerate(patient["questions"]),
             position=1,
@@ -47,7 +67,7 @@ def run_clinical_bert():
                 questions.append(question_str)
                 answers.append(answer)
 
-            qa_stack = embed_questions(questions, answers, model, tokenizer, device)
+            qa_stack = embed_questions(questions, answers, model, device)
 
             # get the top-5 entries in the original document for each question, then 
             # get all the unique ones. This will help us to formulate the best passages
@@ -59,33 +79,55 @@ def run_clinical_bert():
             retrieved_passages = '\n'.join([patient_document[i] for i in unq_indices])
 
             model_prompt = format_prompt(retrieved_passages,question)
-            
-            input_ids = tokenizer_qa.encode(model_prompt, return_tensors="pt", max_length=512, truncation=True)
-            outputs = model_qa.generate(input_ids)
-            response = tokenizer_qa.decode(outputs[0], skip_special_tokens=True)
 
-            print(response)
+            results = model(model_prompt)
 
-def get_patient_doc_matrix(patient, model, tokenizer, device, max_length=256):
+            if start_positions is not None and end_positions is not None:
+                # If we are on multi-GPU, split add a dimension
+                if len(start_positions.size()) > 1:
+                    start_positions = start_positions.squeeze(-1)
+                if len(end_positions.size()) > 1:
+                    end_positions = end_positions.squeeze(-1)
+                # sometimes the start/end positions are outside our model inputs, we ignore these terms
+                ignored_index = start_logits.size(1)
+                start_positions.clamp_(0, ignored_index)
+                end_positions.clamp_(0, ignored_index)
+
+                loss_fct = CrossEntropyLoss(ignore_index=ignored_index)
+                start_loss = loss_fct(start_logits, start_positions)
+                end_loss = loss_fct(end_logits, end_positions)
+                total_loss = (start_loss + end_loss) / 2
+                outputs = (total_loss,) + outputs
+                
+
+
+
+            #input_ids = tokenizer_qa.encode(model_prompt, return_tensors="pt", max_length=512, truncation=True)
+            #outputs = model_qa.generate(input_ids)
+            #response = tokenizer_qa.decode(outputs[0], skip_special_tokens=True)
+
+            #print(response)
+
+def get_patient_doc_matrix(patient, model, device, max_length=256):
     patient_texts = '\n'.join(patient["texts"].values())
     patient_document = break_text_into_passages(patient_texts,max_length)
     passages = []
     for passage in patient_document:
-        embedded = embed_passages(passage,model,tokenizer, device, max_length)
+        embedded = embed_passages(passage ,model, device, max_length)
         passages.append(embedded)
     return torch.cat(passages), patient_document
 
-def embed_passages(passages: 'list[str]', model, tokenizer, device='cpu', max_length=256):
+def embed_passages(passages: 'list[str]', model, device='cpu', max_length=256):
     model.eval()
-    batch = tokenizer(passages,padding=True, max_length=max_length, truncation=True, return_tensors='pt').to(device)
-    results = model(**batch)
+    batch = model.tokenizer(passages,padding=True, max_length=max_length, truncation=True, return_tensors='pt').to(device)
+    results = model.bert(**batch)
     return results.last_hidden_state[:,0,:]
 
-def embed_questions(titles, bodies, model, tokenizer, device='cpu', max_length=512):
+def embed_questions(titles, bodies, model, device='cpu', max_length=512):
     model.eval()
     questions = [(qt,qb) for qt,qb in zip(titles,bodies)]
-    batch = tokenizer(questions,padding=True, max_length=max_length, truncation=True, return_tensors='pt').to(device)
-    results = model(**batch)
+    batch = model.tokenizer(questions,padding=True, max_length=max_length, truncation=True, return_tensors='pt').to(device)
+    results = model.bert(**batch)
     return results.last_hidden_state[:,0,:]
 
 def get_topk_indices(Q, P, k: int = None):
